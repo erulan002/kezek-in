9d7c57c04fdd4fd035ee524e763db68a8ef04c37
erulan <kajyrbekoverulan@gmail.com>
2025-06-13 10:35:41 +0500

    Add: Bottom Nav Bar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:kezek_in/features/home/view/home_screen.dart';
+import 'package:kezek_in/features/profile/view/profile_screen.dart';
+import 'package:kezek_in/features/search/view/search_screen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  List<Widget> _screens = [HomeScreen(), SearchScreen(), ProfileScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Kezek In'), backgroundColor: Colors.teal),
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.teal,
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
